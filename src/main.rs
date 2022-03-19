8e4a25cbb4fd59fed320fc7dd905712fb8cd72e7
DaRealFreak <steffen.keuper@web.de>
2022-03-19 13:01:24 +0100

    [BUGFIX] use wVk for ASCII input instead of wScan UniCode input for proper skill usage

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,29 @@ use std::thread::sleep;
 use std::time;
 
 use windows::Win32::Graphics::Gdi::GetPixel;
-use windows::Win32::UI::Input::KeyboardAndMouse::{GetAsyncKeyState, SendInput};
+use windows::Win32::UI::Input::KeyboardAndMouse::{GetAsyncKeyState, VIRTUAL_KEY, VK_E};
 
 #[cfg(windows)]
-fn send_key(key: char, down: bool) {
+fn send_key(key: VIRTUAL_KEY, down: bool) {
     use winapi::um::winuser::{
-        INPUT, INPUT_KEYBOARD, KEYBDINPUT, KEYEVENTF_KEYUP, KEYEVENTF_UNICODE, SendInput,
+        INPUT, INPUT_KEYBOARD, KEYBDINPUT, KEYEVENTF_KEYUP, SendInput,
     };
 
     let flags = if down { 0 } else { KEYEVENTF_KEYUP };
-    let mut buf = [0; 2];
-
-    for word in key.encode_utf16(&mut buf) {
-        let mut input = INPUT {
-            type_: INPUT_KEYBOARD,
-            u: unsafe {
-                std::mem::transmute_copy(&KEYBDINPUT {
-                    wVk: 0,
-                    wScan: *word,
-                    dwFlags: KEYEVENTF_UNICODE | flags,
-                    time: 0,
-                    dwExtraInfo: 0,
-                })
-            },
-        };
-        unsafe {
-            SendInput(1, &mut input, std::mem::size_of::<INPUT>() as i32);
-        }
+    let mut input = INPUT {
+        type_: INPUT_KEYBOARD,
+        u: unsafe {
+            std::mem::transmute_copy(&KEYBDINPUT {
+                wVk: key.0,
+                wScan: 0,
+                dwFlags: flags,
+                time: 0,
+                dwExtraInfo: 0,
+            })
+        },
+    };
+    unsafe {
+        SendInput(1, &mut input, std::mem::size_of::<INPUT>() as i32);
     }
 }
 
@@ -41,11 +37,20 @@ fn main() {
         loop {
             sleep(time::Duration::from_millis(5));
 
+            // f1
+            if GetAsyncKeyState(0x70) != 0 {}
+
+            // f4
+            if GetAsyncKeyState(0x73) != 0 {
+                exit(0)
+            }
+
+            // f23
             if GetAsyncKeyState(0x86) != 0 {
                 if GetPixel(hdc, 742, 887) == 4331614 {
                     // println!("{}", GetPixel(hdc, 742, 887));
-                    send_key('e', true);
-                    send_key('e', false);
+                    send_key(VK_E, true);
+                    send_key(VK_E, false);
                 }
             }
         }
