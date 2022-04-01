07f7602ae6df86fa6fa6cf560ee3542006a2f07a
DaRealFreak <steffen.keuper@web.de>
2022-04-01 09:28:04 +0200

    [TASK] add function to send unicode strings and keys

--- utility/src/lib.rs
+++ utility/src/lib.rs
@@ -1,8 +1,10 @@
-pub mod game;
-pub mod activity;
+use std::borrow::Borrow;
 
 use windows::Win32::Graphics::Gdi::{GetDC, GetPixel};
-use windows::Win32::UI::Input::KeyboardAndMouse::{GetActiveWindow, INPUT, INPUT_0, INPUT_KEYBOARD, INPUT_MOUSE, KEYBD_EVENT_FLAGS, KEYBDINPUT, KEYEVENTF_KEYUP, MOUSE_EVENT_FLAGS, MOUSEINPUT, SendInput, VIRTUAL_KEY};
+use windows::Win32::UI::Input::KeyboardAndMouse::{GetActiveWindow, INPUT, INPUT_0, INPUT_KEYBOARD, INPUT_MOUSE, KEYBD_EVENT_FLAGS, KEYBDINPUT, KEYEVENTF_KEYUP, KEYEVENTF_UNICODE, MOUSE_EVENT_FLAGS, MOUSEINPUT, SendInput, VIRTUAL_KEY};
+
+pub mod game;
+pub mod activity;
 
 pub unsafe fn get_pixel(x: i32, y: i32) -> String {
     let hwnd = GetActiveWindow();
@@ -41,6 +43,66 @@ pub unsafe fn send_key(key: VIRTUAL_KEY, down: bool) {
                 wVk: key,
                 wScan: 0,
                 dwFlags: flags,
+                time: 0,
+                dwExtraInfo: 0,
+            }
+        },
+    };
+    SendInput(&[input], std::mem::size_of::<INPUT>() as i32);
+}
+
+pub unsafe fn send_string(text: String, unicode: bool) {
+    let mut pinputs: Vec<INPUT> = vec![];
+    let flags = if unicode { KEYEVENTF_UNICODE } else { KEYBD_EVENT_FLAGS(0) };
+    let key_events = vec![KEYBD_EVENT_FLAGS(0), KEYEVENTF_KEYUP];
+
+    let mut chars: Vec<u16> = text.encode_utf16().collect();
+    for char in chars {
+        for key_event in key_events.clone() {
+            if unicode {
+                let input = INPUT {
+                    r#type: INPUT_KEYBOARD,
+                    Anonymous: INPUT_0 {
+                        ki: KEYBDINPUT {
+                            wVk: VIRTUAL_KEY::default(),
+                            wScan: char,
+                            dwFlags: key_event | flags,
+                            time: 0,
+                            dwExtraInfo: 0,
+                        }
+                    },
+                };
+                pinputs.push(input);
+            } else {
+                let input = INPUT {
+                    r#type: INPUT_KEYBOARD,
+                    Anonymous: INPUT_0 {
+                        ki: KEYBDINPUT {
+                            wVk: VIRTUAL_KEY(char),
+                            wScan: 0,
+                            dwFlags: key_event | flags,
+                            time: 0,
+                            dwExtraInfo: 0,
+                        }
+                    },
+                };
+                pinputs.push(input);
+            }
+        }
+    }
+
+    SendInput(pinputs.borrow(), std::mem::size_of::<INPUT>() as i32);
+}
+
+pub unsafe fn send_unicode_key(key: u16, down: bool) {
+    let flags = if down { KEYBD_EVENT_FLAGS(0) } else { KEYEVENTF_KEYUP };
+    let input = INPUT {
+        r#type: INPUT_KEYBOARD,
+        Anonymous: INPUT_0 {
+            ki: KEYBDINPUT {
+                wVk: VIRTUAL_KEY::default(),
+                wScan: key,
+                dwFlags: flags | KEYEVENTF_UNICODE,
                 time: 0,
                 dwExtraInfo: 0,
             }
