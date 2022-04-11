f1dffdb4d7479de814b9952d77e7f65501b51208
DaRealFreak <steffen.keuper@web.de>
2022-04-11 22:32:11 +0200

    [TASK] change timings a little bit

--- macros/src/classes/destroyer/mod.rs
+++ macros/src/classes/destroyer/mod.rs
@@ -127,7 +127,7 @@ impl BnsMacro for Destroyer {
                 }
             }
 
-            sleep(time::Duration::from_millis(50));
+            sleep(time::Duration::from_millis(45));
 
             let mut in_soulburn = false;
 
@@ -142,7 +142,7 @@ impl BnsMacro for Destroyer {
                 }
             }
 
-            sleep(time::Duration::from_millis(90));
+            sleep(time::Duration::from_millis(95));
 
             // sleep 170ms during SB due to awk mc having 18s cd instead of 24s
             // on 40 ms the script would try to anicancel cleave causing a delay after cleave before mc
